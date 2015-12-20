2fb12e134469ddf1a6cb55f1a55dfedcc677a265
nanaju <naokameka@yahoo.co.jp>
2015-12-20 15:10:08 +0900

    authorization 周り

--- becomeGlider/ViewController.swift
+++ becomeGlider/ViewController.swift
@@ -1,59 +1,80 @@
+//
+//  ViewController.swift
+//  becomeGlider
+//
+//  Created by NaoKONDO on 2015/12/17.
+//  Copyright © 2015年 NaoKONDO. All rights reserved.
+//
+
 import UIKit
 import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+
+class ViewController: UIViewController , CLLocationManagerDelegate {
     
-    var lm: CLLocationManager! = nil
-    var longitude: CLLocationDegrees!
-    var latitude: CLLocationDegrees!
+    //現在位置の取得
+    var lm : CLLocationManager!
+
+    //緯度の保持
+    var latitude :CLLocationDegrees!
+    //経度の保持
+    var longitude : CLLocationDegrees!
+    
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+    super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // フィールドの初期化
+        lm = CLLocationManager()
+        // CLLocationManagerをDelegateに指定
+        lm.delegate = self
+
+        // セキュリティ認証のステータスを取得.
+        let status = CLLocationManager.authorizationStatus()
         
-        let url = NSURL(scheme: "http", host: "ofnow-api.herokuapp.com", path: "/1.0/users")
-        let request = NSURLRequest(URL: url!)
-        var json: NSDictionary
-        do {
-            let data = try NSURLConnection.sendSynchronousRequest(request, returningResponse: nil)
-            json = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableLeaves) as! NSDictionary
-        } catch {
-            // エラー処理
+        // まだ認証が得られていない場合は、認証ダイアログを表示.
+        if(status == CLAuthorizationStatus.NotDetermined) {
+            print("didChangeAuthorizationStatus:\(status)");
+            // まだ承認が得られていない場合は、認証ダイアログを表示.
+            lm.requestAlwaysAuthorization()
         }
         
-        longitude = 0.0
-        latitude = 0.0
         
-        lm = CLLocationManager()
-        lm.delegate = self
-        //位置情報取得の可否。バックグラウンドで実行中の場合にもアプリが位置情報を利用することを許可する
-        lm.requestAlwaysAuthorization()
-        //位置情報の精度
-        lm.desiredAccuracy = kCLLocationAccuracyBest
-        //位置情報取得間隔(m)
-        lm.distanceFilter = 300
         
-        // 位置情報を取得
+        
+        // 位置情報の精度を指定．任意，
+         lm.desiredAccuracy = kCLLocationAccuracyBest
+        // 位置情報取得間隔を指定．指定した値（メートル）移動したら位置情報を更新する．任意．
+         lm.distanceFilter = 50
+        
+        // GPSの使用を開始する
         lm.startUpdatingLocation()
-        
     }
-    
+
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    /** 位置情報取得成功時 */
+    /* 位置情報取得成功時に実行される関数 */
     func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation){
-        longitude = newLocation.coordinate.longitude
+        // 取得した緯度がnewLocation.coordinate.longitudeに格納されている
         latitude = newLocation.coordinate.latitude
+        // 取得した経度がnewLocation.coordinate.longitudeに格納されている
+        longitude = newLocation.coordinate.longitude
+        // 取得した緯度・経度をLogに表示
+        print("slatiitude: \(latitude) , longitude: \(longitude)")
         
-        print(String(longitude))
-        print(String(latitude))
+        // GPSの使用を停止する．停止しない限りGPSは実行され，指定間隔で更新され続ける．
+        // lm.stopUpdatingLocation()
     }
-    
-    /** 位置情報取得失敗時 */
+    /* 位置情報取得失敗時に実行される関数 */
     func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
-        NSLog("Error")
+        // この例ではLogにErrorと表示するだけ．
+        print("Error")
+        
     }
-    
+
+
 }
+
